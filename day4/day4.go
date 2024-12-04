2ba7b3877bb53e268d222a7cdbe02e0342008ca4
Mohit Negi <mnegi1433@gmail.com>
2024-12-04 11:42:25 +0530

    Day4 completed

--- day4/day4.go
+++ day4/day4.go
@@ -131,6 +131,30 @@ func count_xmas(row, col int, arr []string) int {
 
 }
 
+func is_x_mas(r, c int, arr []string) bool {
+	val11 := arr[r-1][c-1]
+	val12 := arr[r-1][c+1]
+	val21 := arr[r+1][c-1]
+	val22 := arr[r+1][c+1]
+
+	// fmt.Println(val11, val12, val21, val22)
+
+	str1 := string(val11) + string(val12)
+	str2 := string(val21) + string(val22)
+
+	// fmt.Println(str1, str2)
+
+	if str1 == str2 && (str1 == "MS" || str1 == "SM") {
+		return true
+	}
+
+	if str1 != str2 && ((str1 == "MM" && str2 == "SS") || (str1 == "SS" && str2 == "MM")) {
+		return true
+	}
+
+	return false
+}
+
 func main() {
 	data, err := os.ReadFile("day4_test.txt")
 
@@ -148,8 +172,10 @@ func main() {
 	for r, row := range arr {
 
 		for col, c := range row {
-			if c == 'X' {
-				res += count_xmas(r, col, arr)
+			if c == 'A' && (r > 0 && r < len(arr)-1) && (col > 0 && col < len(arr[0])-1) {
+				if is_x_mas(r, col, arr) {
+					res++
+				}
 			}
 		}
 
