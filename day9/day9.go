ca6ae70c145483f3a4c68f40ad4b057bf4afcafb
Mohit Negi <mnegi1433@gmail.com>
2024-12-09 20:55:13 +0530

    Day9 completed

--- day9/day9.go
+++ day9/day9.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -35,30 +36,111 @@ func main() {
 		id++
 	}
 
+	// <--- part 1 ---> start
+
+	// start := 0
+	// last := len(dec_input) - 1
+
+	// free_space := 0
+	// need := 0
+	// for start < last {
+	// 	for dec_input[start] != "." && free_space == 0 {
+	// 		start++
+	// 	}
+
+	// 	for dec_input[start] == "." {
+	// 		free_space++
+	// 		start++
+	// 	}
+
+	// 	for dec_input[last] == "." && need == 0 {
+	// 		last--
+	// 	}
+	// 	if start >= last {
+	// 		break
+	// 	}
+
+	// 	ref := dec_input[last]
+	// 	for start < last && dec_input[last] != "." && dec_input[last] == ref {
+	// 		need++
+	// 		last--
+	// 	}
+
+	// 	if start >= last {
+	// 		break
+	// 	}
+
+	// 	if free_space >= need {
+	// 		for need != 0 {
+	// 			dec_input[start-free_space] = ref
+	// 			dec_input[last+need] = "."
+	// 			free_space--
+	// 			need--
+	// 		}
+	// 	} else {
+	// 		free_space = 0
+	// 	}
+	// }
 	// fmt.Println(dec_input)
 
-	start := 0
-	last := len(dec_input) - 1
+	// <--- part 1 ---> end
 
-	for start < last {
-		for dec_input[start] != "." {
-			start++
+	// <--- part 2 ---> start
+
+	var sorted_id []int
+	mp := make(map[int]int)
+	for i := len(dec_input) - 1; i >= 0; i-- {
+		if dec_input[i] == "." {
+			continue
 		}
-		for dec_input[last] == "." {
-			last--
+		val, _ := strconv.Atoi(dec_input[i])
+		if mp[val] == 0 {
+			sorted_id = append(sorted_id, val)
 		}
-		if start >= last {
-			break
-		}
-		dec_input[start] = dec_input[last]
-		dec_input[last] = "."
+		mp[val]++
 	}
 
-	// fmt.Println(dec_input)
+	sort.Sort(sort.Reverse(sort.IntSlice(sorted_id)))
+
+	for idx, id := range sorted_id {
+		need := mp[sorted_id[idx]]
+		free_space := 0
+		for i := 0; i < len(dec_input); i++ {
+			if strconv.Itoa(id) == dec_input[i] {
+				break
+			}
+			if dec_input[i] == "." {
+				free_space++
+			} else {
+				free_space = 0
+			}
+
+			if free_space == need {
+				// fmt.Println(id, i, i-free_space+1, free_space)
+				for j := len(dec_input) - 1; j >= 0; j-- {
+					if dec_input[j] == strconv.Itoa(id) {
+						dec_input[j] = "."
+					}
+				}
+				for free_space != 0 {
+					dec_input[i-free_space+1] = strconv.Itoa(id)
+					free_space--
+				}
+				// fmt.Println(dec_input)
+				// fmt.Println()
+
+				break
+			}
+		}
+
+	}
+
+	// <--- part 2 ---> end
+
 	output := 0
 	for pos, val := range dec_input {
 		if val == "." {
-			break
+			continue
 		}
 		id, _ := strconv.Atoi(val)
 		if id < 0 {
